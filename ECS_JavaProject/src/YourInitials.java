e8dcd5fde637f8c4336e23b184f2e2b457b4b9b9
David He <HeD8472@sduhsd.lan>
2017-11-29 11:48:01 -0800

    Finished 3rd Project

---
+++ ECS_JavaProject/src/YourInitials.java
@@ -0,0 +1,12 @@
+public class YourInitials 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("For the name David Li He...");
+		System.out.println("DDDD     L       H    H");
+		System.out.println("D   D    L       H    H");
+		System.out.println("D   D    L       HHHHHH");
+		System.out.println("D   D    L       H    H");
+		System.out.println("DDDD     LLLLL   H    H");
+	}
+}
