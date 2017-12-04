c340f64619ad3288068049b81457ff8f9a7fb1cf
David He <HeD8472@sduhsd.lan>
2017-12-04 10:08:47 -0800

    Done with task 4 assignment 4

---
+++ ECS_JavaProject/src/assignment_4/ALittleQuiz.java
@@ -0,0 +1,66 @@
+package assignment_4;
+import java.util.Scanner;
+public class ALittleQuiz 
+
+{
+	public static void main(String[] args) 
+	{
+		Scanner keyboard = new Scanner(System.in);
+		
+		int ans;
+		
+		System.out.println("What is the capital of Alaska?");
+		System.out.println("    1) Melbourne");
+		System.out.println("    2) Anchorage");
+		System.out.println("    3) Juneau");
+		ans = keyboard.nextInt();
+		if (ans == 1) 
+		{
+			System.out.println("I can tell someone didn't pass geography. ");
+		}
+		if (ans == 2) 
+		{
+			System.out.println("I can tell someone didn't pass geography. ");
+		}
+		if (ans == 3) 
+		{
+			System.out.println("<o/ <o/ <o/");
+		}
+		System.out.println("What's 9 + 10?");
+		System.out.println("    1) 21");
+		System.out.println("    2) 19");
+		System.out.println("    3) Cabbage");
+		ans = keyboard.nextInt();
+		if (ans == 1) 
+		{
+			System.out.println("YEEE!");
+		}
+		if (ans == 2) 
+		{
+			System.out.println("Nerd. Correct, i guess... ");
+		}
+		if (ans == 3) 
+		{
+			System.out.println("I don't know where to begin with this one...");
+		}
+		System.out.println("Which of these is a move that Salamence can use? ");
+		System.out.println("    1) Hydro Pump");
+		System.out.println("    2) Close Combat");
+		System.out.println("    3) Splash");
+		ans = keyboard.nextInt();
+		if (ans == 1) 
+		{
+			System.out.println("Special salamence FTW! Correct!");
+		}
+		if (ans == 2) 
+		{
+			System.out.println(".........");
+		}
+		if (ans == 3) 
+		{
+			System.out.println("What are you doing, Mantine Surf? ");
+		}
+		keyboard.close();
+	}
+	
+}
