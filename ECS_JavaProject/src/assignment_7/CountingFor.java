3bf2f1e3a9e1184c838594d65b3c546ab24fd0dd
David He <HeD8472@sduhsd.lan>
2017-12-07 09:38:13 -0800

    done with task 1 assignment 7

---
+++ ECS_JavaProject/src/assignment_7/CountingFor.java
@@ -0,0 +1,18 @@
+package assignment_7;
+import java.util.Scanner;
+public class CountingFor {
+	public static void main( String[] args )
+	{
+		Scanner keyboard = new Scanner (System.in);
+		System.out.println( "Type in a message, and I'll display it 5 times.");
+		
+		System.out.print( "Message: ");
+		String message = keyboard.nextLine();
+		
+		for ( int n = 1 ; n <= 5 ; n = n + 1 )
+		{
+			System.out.println( n + ". " + message);
+		}
+		keyboard.close();
+	}
+}
